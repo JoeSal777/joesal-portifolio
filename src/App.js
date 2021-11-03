526a232328c2a19203b753412307d9d6ecb2eeff
josephsalem <larkai.joseph@gmail.com>
2021-11-03 00:04:30 +0000

    testing react-fragment

--- src/App.js
+++ src/App.js
@@ -5,16 +5,9 @@ import React, { Component } from 'react';
 class App extends Component{
   render(){
     return(
-      <div className="container">
-        <div className="row">
-          <div className="col-6"> column number 1</div>
-          <div className="col-6"> 
-            <span>
-              <i className="fas fa-home"/>
-            </span>
-            </div>
-        </div>
-      </div>
+      <React.Fragment>
+        <h3>heloooooo</h3>
+    </React.Fragment>
     );
   }
 }
