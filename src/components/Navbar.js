3e7ef80df578e3f2a7e32aa236c3952ea1cd7600
josephsalem <larkai.joseph@gmail.com>
2021-11-04 00:01:07 +0000

    adding more navbar code but all commented out

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,12 +7,57 @@ import React, { Component } from 'react';
 
 class Navbar extends Component {
     
+    // state={
+    //     isOpen: false
+    // }
+    // handleToggle = () =>{
+    //     this.setState({isOpen:!this.state.isOpen})
+    // }
     render() {
         return (
             <nav className="navbar">
-               <div>
-                    <h3>position top and left commented</h3>
-                </div> 
+                <div>
+                <h3>hello from the navbar us page</h3>
+                </div>
+                {/* <div className="nav-center"> */}
+                    {/* <div className="nav-header"> */}
+
+                        {/* For logo */}
+                        {/* <Link to="/">
+                            <img src={logo} alt="Joseph Larkai Logo" />
+                        </Link> */}
+
+                        {/* For contact-me icon in mobile view */}
+                        {/* <Link to="/contact" className="nav-btn-small-screen">
+                            <button type="button">
+                                <FiMail className="Nav-icon" />   
+                            </button>
+                        </Link> */}
+
+                        {/* For menu icon in mobile view */}
+                        {/* <button type="button" className="nav-btn-small-screen" onClick={this.handleToggle}>
+                            <FaAlignRight className="Nav-icon" />   
+                        </button> 
+                    </div> */}
+
+                    {/* For nav menu */}
+                    {/* <ul className={this.state.isOpen ? "nav-links show-nav" : "nav-links"}>
+                        <li>
+                            <Link to="/resume">Resume.</Link>
+                        </li>
+                        <li>
+                            <Link to="/projects">Projects.</Link>
+                        </li>
+                    </ul> */}
+
+                    {/* For contact me icon in nav-bar */}
+                    {/* <Link to="/contact" className="nav-btn-big-screen">
+                        <button type="button">
+                            <FiMail className="Nav-icon" />   
+                        </button>
+                    </Link> */}
+
+                {/* </div> */}
             </nav>
         );
     }
