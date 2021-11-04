aef1161a4ecf377f5b52976d0aea20058539331a
josephsalem <larkai.joseph@gmail.com>
2021-11-04 22:18:40 +0000

    trouble shooting title

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -19,7 +19,7 @@ class LandingPage extends Component {
 
                 {/* title */}
                 <div className="title-wrapper tittle-element">
-                    <div className="title">
+                    <div className="titlee">
                         <h1>Joseph Salem Mensah-Larkai</h1>
                     </div>
                 </div>
