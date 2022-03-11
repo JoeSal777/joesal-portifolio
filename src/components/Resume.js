e22be8184857fc1e36e27de01ae929adf694860c
josephsalem <larkai.joseph@gmail.com>
2022-03-11 02:17:37 +0000

    final touches to left side of resume

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -34,9 +34,11 @@ export default class Resume extends Component {
                                         <FaUniversity className="history-icon"/>
                                     </div>
                                     <div className="info-item-history history-items">
-                                        <div className="history-item">
-                                            <IconInfoBox title={"University of Ghana · Undergrad · 2013-2017"} text={"BSc Mathematics"}/>
-                                        </div>
+                                        <a href="https://www.ug.edu.gh/" target="_blank">
+                                            <div className="history-item">
+                                                <IconInfoBox title={"University of Ghana · Undergrad · 2013-2017"} text={"BSc Mathematics"}/>
+                                            </div>
+                                        </a>
                                     </div>
                                 </div>
 
@@ -45,12 +47,16 @@ export default class Resume extends Component {
                                         <MdOutlineWorkOutline className="history-icon"/>
                                     </div>
                                     <div className="info-item-history history-items">
-                                        <div className="history-item">
-                                            <IconInfoBox title={"Phoenix Life Assurance · Intern · 2016"} text={"Insurance Officer"}/>
-                                        </div>
-                                        <div className="history-item">
-                                            <IconInfoBox title={"Ga-East Municipal Assembly · National Service personnel · 2017-2018"} text={"Ass. Budget Officer"}/>
-                                        </div>
+                                        <a href="https://assurance.cdhgroup.co/" target="_blank">
+                                            <div className="history-item">
+                                                <IconInfoBox title={"Phoenix Life Assurance · Intern · 2016"} text={"Insurance Officer"}/>
+                                            </div>
+                                        </a>
+                                        <a href="http://gema.gov.gh/" target="_blank">
+                                            <div className="history-item">
+                                                <IconInfoBox title={"Ga-East Municipal Assembly · National Service personnel · 2017-2018"} text={"Ass. Budget Officer"}/>
+                                            </div>
+                                        </a>
                                         <div className="history-item">
                                             <IconInfoBox title={"QBE Ghana Limited · Part-time · 2019-Present"} text={"Programmer"}/>
                                         </div>
